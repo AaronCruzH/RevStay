e86b3b7ff20dec6cf85d9efd70a761de994a501c
RodrigoPimienta <capitanpimienta28@gmail.com>
2025-04-01 18:06:21 -0600

    feat(repository): add new method to get HotelAmenity by hotel
    add new mehotd to get a specific hotelAmenity by id and his hotelId

--- src/main/java/com/revature/stay/repos/HotelAmenityDAO.java
+++ src/main/java/com/revature/stay/repos/HotelAmenityDAO.java
@@ -7,9 +7,12 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface HotelAmenityDAO extends JpaRepository<HotelAmenity, Integer> {
     @Query("SELECT ha FROM HotelAmenity ha WHERE ha.hotel.hotelId = :hotelId")
     List<HotelAmenity> findByHotelId(@Param("hotelId") int hotelId);
+
+    Optional<HotelAmenity> findByHotelAmenityIdAndHotel_HotelId(int hotelAmenityId, int hotelId);
 }
