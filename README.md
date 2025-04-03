4cd9df7ea73e6f0415a7957791a3c8c4c7e3a93a
Aaron Cruz <92279294+AaronCruzH@users.noreply.github.com>
2025-04-03 03:39:01 -0600

    Adding ReservationService Test

--- README.md
+++ README.md
@@ -11,19 +11,19 @@ functional and intuitive application, a technical presentation, and associated d
 ### User account:
 As a user, I should be able to:
 
-1. Register on the platform. *
+1. Register on the platform. * **C**
 
-2. Login into the application using email and password. *
+2. Login into the application using email and password. * **C**
 
 3. Search and view hotel details including images, location, price, amenities, and
-user reviews. *
+user reviews. * **R**
 
-4. Filter hotels based on criteria such as location and price range.
+4. Filter hotels based on criteria such as location and price range. **R**
 
 5. Make hotel reservations by selecting check-in and check-out dates, room type,
-and number of guests. *
+and number of guests. * **A**
 
-6. View and manage my bookings, including modifying or canceling reservations. *
+6. View and manage my bookings, including modifying or canceling reservations. * **A**
 
 7. Receive email or push notifications for booking confirmations and updates.
 
@@ -39,16 +39,16 @@ and number of guests. *
 ### Hotel Owner Account:
 As a hotel owner, I should be able to:
 
-1. Register as a seller with email, password, and business details. *
+1. Register as a seller with email, password, and business details. * **C**
 
-2. Login into the application using email and password. *
+2. Login into the application using email and password. * **C**
 
 3. Manage hotel listings and details, including images, descriptions, pricing, and
-availability. *
+availability. * **E**
 
 4. Receive notifications for new bookings and reservation updates.
 
-5. View and manage reservations, including accepting or rejecting bookings. *
+5. View and manage reservations, including accepting or rejecting bookings. * **A** 
 
 6. Respond to user reviews and feedback.
 
---
+++ src/main/java/com/revature/stay/models/Reservation.java
@@ -0,0 +1,135 @@
+package com.revature.stay.models;
+
+import ch.qos.logback.core.net.server.Client;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "reservations")
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int reservationId;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    @Column(nullable = false)
+    private int totalGuests;
+
+    @Column(nullable = false)
+    private Date checkIn;
+
+    @Column(nullable = false)
+    private Date checkOut;
+
+    @Column(nullable = false)
+    private Date createdAt;
+
+    @Column(nullable = false)
+    private float total;
+
+    @Column(nullable = false)
+    private ReservationStatus reservationStatus;
+
+    public Reservation() {
+    }
+
+    public Reservation(int reservationId, Client client, Room room, int totalGuests, Date checkIn, Date checkOut, Date createdAt, float total, ReservationStatus reservationStatus) {
+        this.reservationId = reservationId;
+        this.client = client;
+        this.room = room;
+        this.totalGuests = totalGuests;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        this.createdAt = createdAt;
+        this.total = total;
+        this.reservationStatus = reservationStatus;
+    }
+
+    public Reservation(Client client, Room room, int totalGuests, Date checkIn, Date checkOut) {
+        this.client = client;
+        this.room = room;
+        this.totalGuests = totalGuests;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(Date checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public int getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(int reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public int getTotalGuests() {
+        return totalGuests;
+    }
+
+    public void setTotalGuests(int totalGuests) {
+        this.totalGuests = totalGuests;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(Date checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
+    }
+
+    public ReservationStatus getReservationStatus() {
+        return reservationStatus;
+    }
+
+    public void setReservationStatus(ReservationStatus reservationStatus) {
+        this.reservationStatus = reservationStatus;
+    }
+}
